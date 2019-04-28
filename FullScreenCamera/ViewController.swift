abdb3df4bcf1c4335d48d785bb7e3f9a2bf2ef74
joonwon lee <cafielo@gmail.com>
2019-04-28 14:00:42 +0900

    Initial Commit

---
+++ FullScreenCamera/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FullScreenCamera
+//
+//  Created by joonwon lee on 28/04/2019.
+//  Copyright © 2019 com.joonwon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
